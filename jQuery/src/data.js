436345246f65c169a7420a621e76f3abe654940e
flagmanAndrew <andrey.sergeyev@devexpress.com>
2025-11-10 17:47:40 +0400

    jquery

---
+++ jQuery/src/data.js
@@ -0,0 +1,267 @@
+const data = [
+  {
+    'text': 'Brochure Design Review',
+    'startDate': new Date(2022, 9, 1),
+    'endDate': new Date(2022, 9, 1),
+  },
+  {
+    'text': 'Website Re-Design Plan',
+    'startDate': new Date(2022, 9, 1),
+    'endDate': new Date(2022, 9, 1),
+  },
+  {
+    'text': 'Rollout of New Website and Marketing Brochures',
+    'startDate': new Date(2022, 9, 1),
+    'endDate': new Date(2022, 9, 1),
+  },
+  {
+    'text': 'Update NDA Agreement',
+    'startDate': new Date(2022, 9, 2),
+    'endDate': new Date(2022, 9, 2),
+  },
+  {
+    'text': 'Update Employee Files with New NDA',
+    'startDate': new Date(2022, 9, 2),
+    'endDate': new Date(2022, 9, 2),
+  },
+  {
+    'text': 'Comment on Revenue Projections',
+    'startDate': new Date(2022, 9, 3),
+    'endDate': new Date(2022, 9, 3),
+  },
+  {
+    'text': 'Provide New Health Insurance Docs',
+    'startDate': new Date(2022, 9, 3),
+    'endDate': new Date(2022, 9, 3),
+  },
+  {
+    'text': 'Review Changes to Health Insurance Coverage',
+    'startDate': new Date(2022, 9, 3),
+    'endDate': new Date(2022, 9, 3),
+  },
+  {
+    'text': 'Review Customer Feedback Report',
+    'startDate': new Date(2022, 9, 4),
+    'endDate': new Date(2022, 9, 4),
+  },
+  {
+    'text': 'Customer Feedback Report Analysis',
+    'startDate': new Date(2022, 9, 4),
+    'endDate': new Date(2022, 9, 4),
+  },
+  {
+    'text': 'Prepare Shipping Cost Analysis Report',
+    'startDate': new Date(2022, 9, 4),
+    'endDate': new Date(2022, 9, 4),
+  },
+  {
+    'text': 'Get Design for Shipping Return Labels',
+    'startDate': new Date(2022, 9, 7),
+    'endDate': new Date(2022, 9, 8),
+  },
+  {
+    'text': 'PSD needed for Shipping Return Labels',
+    'startDate': new Date(2022, 9, 7),
+    'endDate': new Date(2022, 9, 7),
+  },
+  {
+    'text': 'Contact ISP and Discuss Payment Options',
+    'startDate': new Date(2022, 9, 7),
+    'endDate': new Date(2022, 9, 7),
+  },
+  {
+    'text': 'Training Material Distribution Schedule',
+    'startDate': new Date(2022, 9, 8),
+    'endDate': new Date(2022, 9, 8),
+  },
+  {
+    'text': 'Create New Spike for Automation Server',
+    'startDate': new Date(2022, 9, 8),
+    'endDate': new Date(2022, 9, 8),
+  },
+  {
+    'text': 'Send 2 Remotes for Giveaways',
+    'startDate': new Date(2022, 9, 8),
+    'endDate': new Date(2022, 9, 10),
+  },
+  {
+    'text': 'Discuss Product Giveaways with Management',
+    'startDate': new Date(2022, 9, 9),
+    'endDate': new Date(2022, 9, 9),
+  },
+  {
+    'text': 'Replace Desktops on the 3rd Floor',
+    'startDate': new Date(2022, 9, 9),
+    'endDate': new Date(2022, 9, 9),
+  },
+  {
+    'text': 'Territory Sales Breakdown Report',
+    'startDate': new Date(2022, 9, 10),
+    'endDate': new Date(2022, 9, 10),
+  },
+  {
+    'text': 'Report on the State of Engineering Dept',
+    'startDate': new Date(2022, 9, 10),
+    'endDate': new Date(2022, 9, 10),
+  },
+  {
+    'text': 'Brochure Design Review',
+    'startDate': new Date(2022, 9, 11),
+    'endDate': new Date(2022, 9, 11),
+  },
+  {
+    'text': 'Website Re-Design Plan',
+    'startDate': new Date(2022, 9, 11),
+    'endDate': new Date(2022, 9, 11),
+  },
+  {
+    'text': 'Customer Feedback Report Analysis',
+    'startDate': new Date(2022, 9, 14),
+    'endDate': new Date(2022, 9, 14),
+  },
+  {
+    'text': 'Prepare Shipping Cost Analysis Report',
+    'startDate': new Date(2022, 9, 14),
+    'endDate': new Date(2022, 9, 14),
+  },
+  {
+    'text': 'Upgrade Server Hardware',
+    'startDate': new Date(2022, 9, 15),
+    'endDate': new Date(2022, 9, 15),
+  },
+  {
+    'text': 'Upgrade Personal Computers',
+    'startDate': new Date(2022, 9, 15),
+    'endDate': new Date(2022, 9, 15),
+  },
+  {
+    'text': 'Upgrade Apps to Windows RT or stay with WinForms',
+    'startDate': new Date(2022, 9, 15),
+    'endDate': new Date(2022, 9, 15),
+  },
+  {
+    'text': 'Create Icons for Website',
+    'startDate': new Date(2022, 9, 16),
+    'endDate': new Date(2022, 9, 16),
+  },
+  {
+    'text': 'Create New Product Pages',
+    'startDate': new Date(2022, 9, 17),
+    'endDate': new Date(2022, 9, 17),
+  },
+  {
+    'text': 'Approve Website Launch',
+    'startDate': new Date(2022, 9, 16),
+    'endDate': new Date(2022, 9, 16),
+  },
+  {
+    'text': 'PSD needed for Shipping Return Labels',
+    'startDate': new Date(2022, 9, 17),
+    'endDate': new Date(2022, 9, 17),
+
+  },
+  {
+    'text': 'Contact ISP and Discuss Payment Options',
+    'startDate': new Date(2022, 9, 13),
+    'endDate': new Date(2022, 9, 13),
+
+  },
+  {
+    'text': 'Prepare Year-End Support Summary Report',
+    'startDate': new Date(2022, 9, 27),
+    'endDate': new Date(2022, 9, 27),
+
+  },
+  {
+    'text': 'Approval on Converting to New HDMI Specification',
+    'startDate': new Date(2022, 9, 18),
+    'endDate': new Date(2022, 9, 18),
+
+  },
+  {
+    'text': 'Create New Spike for Automation Server',
+    'startDate': new Date(2022, 9, 18),
+    'endDate': new Date(2022, 9, 18),
+
+  },
+  {
+    'text': 'Send 2 Remotes for Giveaways',
+    'startDate': new Date(2022, 9, 19),
+    'endDate': new Date(2022, 9, 19),
+  },
+  {
+    'text': 'Website Re-Design Plan',
+    'startDate': new Date(2022, 9, 21),
+    'endDate': new Date(2022, 9, 21),
+  },
+  {
+    'text': 'Rollout of New Website and Marketing Brochures',
+    'startDate': new Date(2022, 9, 21),
+    'endDate': new Date(2022, 9, 21),
+  },
+  {
+    'text': 'Update Sales Strategy Documents',
+    'startDate': new Date(2022, 9, 21),
+    'endDate': new Date(2022, 9, 21),
+  },
+  {
+    'text': 'Update Employee Files with New NDA',
+    'startDate': new Date(2022, 9, 22),
+    'endDate': new Date(2022, 9, 22),
+  },
+  {
+    'text': 'Submit Questions Regarding New NDA',
+    'startDate': new Date(2022, 9, 22),
+    'endDate': new Date(2022, 9, 22),
+  },
+  {
+    'text': 'Provide New Health Insurance Docs',
+    'startDate': new Date(2022, 9, 23),
+    'endDate': new Date(2022, 9, 23),
+  },
+  {
+    'text': 'Review Changes to Health Insurance Coverage',
+    'startDate': new Date(2022, 9, 23),
+    'endDate': new Date(2022, 9, 23),
+  },
+  {
+    'text': 'Review Training Course for any Ommissions',
+    'startDate': new Date(2022, 9, 20),
+    'endDate': new Date(2022, 9, 20),
+  },
+  {
+    'text': 'Customer Feedback Report Analysis',
+    'startDate': new Date(2022, 9, 24),
+    'endDate': new Date(2022, 9, 24),
+  },
+  {
+    'text': 'Prepare Shipping Cost Analysis Report',
+    'startDate': new Date(2022, 9, 24),
+    'endDate': new Date(2022, 9, 24),
+  },
+  {
+    'text': 'Provide Feedback on Shippers',
+    'startDate': new Date(2022, 9, 24),
+    'endDate': new Date(2022, 9, 24),
+  },
+  {
+    'text': 'Upgrade Personal Computers',
+    'startDate': new Date(2022, 9, 25),
+    'endDate': new Date(2022, 9, 25),
+  },
+  {
+    'text': 'Upgrade Apps to Windows RT or stay with WinForms',
+    'startDate': new Date(2022, 9, 25),
+    'endDate': new Date(2022, 9, 25),
+  },
+  {
+    'text': 'Estimate Time Required to Touch-Enable Apps',
+    'startDate': new Date(2022, 9, 25),
+    'endDate': new Date(2022, 9, 25),
+  },
+  {
+    'text': 'Approve Website Launch',
+    'startDate': new Date(2022, 10, 25),
+    'endDate': new Date(2022, 10, 25),
+  },
+];
--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,97 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  const filterEditorValueChanged = () => {
+    filterAppointments(
+      form.getEditor('text').option('value'),
+      form.getEditor('startDate').option('value'),
+      form.getEditor('endDate').option('value'),
+    );
+  };
+
+  const filterTextBoxSettings = {
+    dataField: 'text',
+    editorType: 'dxTextBox',
+    label: {
+      text: 'Text',
     },
-  });
+    value: '',
+    editorOptions: {
+      placeholder: 'Filter...',
+      valueChangeEvent: 'keyup',
+      onValueChanged: filterEditorValueChanged,
+    },
+  };
+  const startDateSettings = {
+    dataField: 'startDate',
+    editorType: 'dxDateBox',
+    value: new Date(2022, 9, 1),
+    label: {
+      text: 'Start Date',
+    },
+    editorOptions: {
+      type: 'date',
+      onValueChanged: filterEditorValueChanged,
+    },
+  };
+  const endDateSettings = {
+    dataField: 'endDate',
+    editorType: 'dxDateBox',
+    value: new Date(2022, 9, 28),
+    label: {
+      text: 'End Date',
+    },
+    editorOptions: {
+      type: 'date',
+      onValueChanged: filterEditorValueChanged,
+    },
+  };
+
+  const form = $('#form').dxForm({
+    formData: defaultFormData,
+    colCount: 1,
+    labelMode: 'outside',
+    items: [{
+      itemType: 'group',
+      caption: 'Filter Appointments',
+      items: [
+        filterTextBoxSettings,
+        startDateSettings,
+        endDateSettings,
+      ],
+    }],
+  }).dxForm('instance');
+
+  $('#scheduler').dxScheduler({
+    dataSource,
+    views: [{
+      type: 'month',
+    }],
+    width: '80%',
+    currentView: 'month',
+    currentDate: new Date(2022, 9, 1),
+  }).dxScheduler('instance');
+});
+
+const filterAppointments = (filterValue, startDate, endDate) => {
+  dataSource.filter([
+    ['text', 'contains', filterValue],
+    'and',
+    ['startDate', '>=', startDate],
+    'and',
+    ['endDate', '<=', endDate],
+  ]);
+  dataSource.load();
+};
+
+const defaultFormData = {
+  text: '',
+  startDate: new Date(2022, 9, 1),
+  endDate: new Date(2022, 9, 28),
+};
+
+const dataSource = new DevExpress.data.DataSource({
+  store: {
+    type: 'array',
+    data,
+  },
+  paginate: false,
 });
